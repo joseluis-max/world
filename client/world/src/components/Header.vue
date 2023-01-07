5e2440ea31142c796959ff88a1d78b94e245ced7
Joseph <luisj.valdiviezo20@gmail.com>
2023-01-07 09:02:47 -0500

    Add: update

---
+++ client/world/src/components/Header.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header class="header">
+        <h1 class="header--title title">World</h1>
+        
+        <nav class="header--menu">
+            <li class="header--link">
+                <RouterLink to="/wall">Wall</RouterLink>
+            </li>
+            <li class="header--link">
+                <RouterLink to="/profile">profile</RouterLink>
+            </li>
+        </nav>
+
+    </header>
+
+</template>
+
+<style scoped>
+@import '../assets/base.css';
+
+.header {
+    display: flex;
+    height: 70px;
+}
+
+.header--title {
+    width: 100px;
+    height: 50px;
+    text-align: center;
+    margin: 10px 0px 10px 50px;
+}
+.header--menu {
+    display: flex;
+    margin-left: 350px;
+    align-items: center;
+}
+
+.header--link {
+    list-style: none;
+    margin: 0px 30px;
+    color: var(--color-text-dark);
+}
+
+.header--link:hover {
+    text-decoration: underline;
+    cursor: pointer;
+    color: var(--color-text-dark);
+}
+
+
+</style>
---
+++ client/world/src/views/WallView.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import Header from "../components/Header.vue";
+import { ref } from "vue";
+
+export type FileEventTarget = EventTarget & { files: FileList };
+
+const content = ref();
+const listImages: (string | ArrayBuffer | null)[] = [];
+const listVideos: (string | ArrayBuffer | null)[] = [];
+
+const onUploadImages = (ev: Event) => {
+    const target: HTMLInputElement | null = ev.target as HTMLInputElement;
+    if (target.files != null) {
+        for (const file of target.files) {
+            const fileReader = new FileReader();
+            fileReader.readAsArrayBuffer(file);
+            fileReader.onloadend = () => {
+                listImages.push(fileReader.result);
+            }
+        }
+    }
+}
+
+const onUploadVideos = (ev: Event) => {
+    const target: HTMLInputElement | null = ev.target as HTMLInputElement;
+    if (target.files != null) {
+        for (const file of target.files) {
+            const fileReader = new FileReader();
+            fileReader.readAsArrayBuffer(file);
+            fileReader.onloadend = () => {
+                listVideos.push(fileReader.result);
+            }
+        }
+    }
+}
+
+const onClickUpload = (ev: Event) => {
+    ev.preventDefault();
+    console.log(listImages);
+    console.log(listVideos);
+    console.log(content);
+}
+
+</script>
+
+<template>
+    <Header />
+    <div class="wall">
+        <div class="wall--wrapper--form">
+            <form class="wall--form" action="">
+                <textarea class="wall--form--text" v-model="content" name="" id="" cols="100" rows="10" placeholder="Write here... !"></textarea>
+                <input class="wall-form--file button" @change="onUploadImages"  type="file" name="" id="image" accept="image/png, image/jpeg" multiple>
+                <input class="wall-form--file button" @change="onUploadVideos" type="file" name="" id="videos" accept=".video/mp4" multiple>
+                <button class="wall--form--button button" @click="onClickUpload"  type="submit">Upload</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.wall {
+    display: block;
+}
+
+.wall--wrapper--form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.wall-form {
+    display: block;
+}
+
+.wall--form--text {
+    display: block;
+    resize: none;
+    margin-bottom: 10px;
+}
+
+.wall-form--file {
+    margin-right: 10px;
+}
+
+input[type="file"]::file-selector-button {
+    font-weight: bold;
+    padding: 3px 5px;
+}
+
+
+.wall--form--button {
+    margin-left: 156px;
+    padding: 5px 15px;
+}
+</style>
